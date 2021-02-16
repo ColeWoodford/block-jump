6c193bd46229087a0910fa87480a24f24f177d16
ColeWoodford <woodford.cole@gmail.com>
2021-02-16 06:43:35 -0800

    Change serve path.

--- server/index.js
+++ server/index.js
@@ -4,12 +4,12 @@ const path = require("path");
 const port = process.env.PORT || 5000;
 
 //Static file declaration
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 //production mode
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
   app.get("*", (req, res) => {
-    res.sendfile(path.join((__dirname = "client/build/index.html")));
+    res.sendfile(path.join((__dirname = "./client/build/index.html")));
   });
 } else {
   //build mode
