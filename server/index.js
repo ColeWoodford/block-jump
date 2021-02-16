4a4b7ceee3f2eefe9755950da50493a93a31e171
ColeWoodford <woodford.cole@gmail.com>
2021-02-16 06:51:50 -0800

    Bunch of typos from the guide online fixed.

--- server/index.js
+++ server/index.js
@@ -3,17 +3,17 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
 
-//Static file declaration
-app.use(express.static(path.join(__dirname, "./client/build")));
 //production mode
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
-  app.get("*", (req, res) => {
-    res.sendfile(path.join((__dirname = "./client/build/index.html")));
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
   });
 } else {
   //build mode
-  app.get("*", (req, res) => {
+  //Static file declaration
+  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname + "/client/public/index.html"));
   });
 }
