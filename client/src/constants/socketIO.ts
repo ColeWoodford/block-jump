d8f0f27ae4ff4405069e6f2d88fa96e5797d30b2
ColeWoodford <woodford.cole@gmail.com>
2021-02-16 20:12:15 -0800

    change socket url to a production value.

--- client/src/constants/socketIO.ts
+++ client/src/constants/socketIO.ts
@@ -1,4 +1,6 @@
-export const SOCKET_SERVER_URL = "http://localhost:5000";
+// export const SOCKET_SERVER_URL = "http://localhost:5000";
+export const SOCKET_SERVER_URL =
+  "https://powerful-tundra-37029.herokuapp.com/socket.io/?EIO=4&transport=websocket";
 
 export const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
 export const NEW_SCORE_MESSAGE_EVENT = "newScoreMessage";
